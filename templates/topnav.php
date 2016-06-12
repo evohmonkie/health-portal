03e75df992729f22c7d2fceef89d6d58ac40fa84
tlee <evohmonkie@gmail.com>
2016-06-11 22:47:24 -0500

    added navbar and sidebar including initial styling

--- templates/topnav.php
+++
@@ -1,8 +0,0 @@
-<html>
-<ul class="topnav">
-	<li><a href="#home">Home</a></li>
-	<li><a href="#Place Order">Place Order</a></li>
-	<li><a href="#View Orders">View Orders</a></li>
-	<li><a href="#User Profile">User Profile</a></li>
-</ul>
-</html>
