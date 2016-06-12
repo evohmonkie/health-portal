b49ac32bcbf353d45b115dc653af297bd06a2781
Teng Lee <evohmonkie@gmail.com>
2016-06-11 20:08:00 -0500

    added bootstrap and associated libraries

---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+function checkLogin() {
+	var u = document.getElementById("username").value;
+
+	document.getElementById("info").innerHTML = u;
+}
--- login.php
+++ login.php
@@ -2,8 +2,13 @@
 <html>
 <head>
 	<title>Health Portal - Login</title>
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<script src="js/bootstrap.js"></script>
+	<script src="js/main.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 </head>
 <body>
 
 </body>
-</html>
+</html> 
